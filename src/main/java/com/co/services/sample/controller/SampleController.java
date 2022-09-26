7e917c53707424a6b82bd0a2473a5eae7871d7d8
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-26 17:41:21 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -11,14 +11,14 @@ public class SampleController {
 	@GetMapping("/")
 	public Mono<String> sample1() {
 		WebClient webClient = WebClient.create();
-		Mono<String> ms1 = webClient.get().uri("http://localhost:9002/").retrieve().bodyToMono(String.class);
-		Mono<String> ms2 = webClient.get().uri("http://localhost:9003/").retrieve().bodyToMono(String.class);
-		Mono<String> ms3 = webClient.get().uri("http://localhost:9004/").retrieve().bodyToMono(String.class);
-		Mono<String> ms4 = webClient.get().uri("http://localhost:9005/").retrieve().bodyToMono(String.class);
-		Mono<String> ms5 = webClient.get().uri("http://localhost:9006/").retrieve().bodyToMono(String.class);
-		Mono<String> ms6 = webClient.get().uri("http://localhost:9007/").retrieve().bodyToMono(String.class);
-		Mono<String> ms7 = webClient.get().uri("http://localhost:9008/").retrieve().bodyToMono(String.class);
-		Mono<String> ms8 = webClient.get().uri("http://localhost:9009/").retrieve().bodyToMono(String.class);
+		Mono<String> ms1 = webClient.get().uri("sample-service2-v2.charles3.svc.cluster.local").retrieve().bodyToMono(String.class);
+// 		Mono<String> ms2 = webClient.get().uri("http://localhost:9003/").retrieve().bodyToMono(String.class);
+// 		Mono<String> ms3 = webClient.get().uri("http://localhost:9004/").retrieve().bodyToMono(String.class);
+// 		Mono<String> ms4 = webClient.get().uri("http://localhost:9005/").retrieve().bodyToMono(String.class);
+// 		Mono<String> ms5 = webClient.get().uri("http://localhost:9006/").retrieve().bodyToMono(String.class);
+// 		Mono<String> ms6 = webClient.get().uri("http://localhost:9007/").retrieve().bodyToMono(String.class);
+// 		Mono<String> ms7 = webClient.get().uri("http://localhost:9008/").retrieve().bodyToMono(String.class);
+// 		Mono<String> ms8 = webClient.get().uri("http://localhost:9009/").retrieve().bodyToMono(String.class);
 	
 		return Mono.zip(ms1, ms2, ms3, ms4, ms5, ms6, ms7, ms8).flatMap(data -> {
 			return Mono.just("" + data.getT1() + "<br/>" + data.getT2() + "<br/>" + data.getT3() + "<br/>" + data.getT4() + "<br/>" + data.getT5() + "<br/>" + data.getT6() + "<br/>" + data.getT7() + "<br/>" + data.getT8());
