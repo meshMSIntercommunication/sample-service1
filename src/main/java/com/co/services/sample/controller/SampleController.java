8d78c0a2cdf1620537e48eeffaf16ce11436f7ec
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-26 17:58:47 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -19,7 +19,7 @@ public class SampleController {
 // 		Mono<String> ms6 = webClient.get().uri("http://localhost:9007/").retrieve().bodyToMono(String.class);
 // 		Mono<String> ms7 = webClient.get().uri("http://localhost:9008/").retrieve().bodyToMono(String.class);
 // 		Mono<String> ms8 = webClient.get().uri("http://localhost:9009/").retrieve().bodyToMono(String.class);
-		return Mono.zip(ms1, ms2, ms3, ms4, ms5, ms6, ms7, ms8).flatMap(data -> {
+		return Mono.zip(ms1).flatMap(data -> {
 			return Mono.just("" + data.getT1() + "<br/>");
 		});
 // 		return Mono.zip(ms1, ms2, ms3, ms4, ms5, ms6, ms7, ms8).flatMap(data -> {
