ac8352fa3ce7bbec2333771864f4b4dbe2da0529
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-27 13:51:35 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -14,13 +14,13 @@ public class SampleController {
 		Mono<String> ms1 = webClient.get().uri("sample-service2-v2.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
 		Mono<String> ms2 = webClient.get().uri("sample-service3-v1.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
 		Mono<String> ms3 = webClient.get().uri("sample-service4.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-// 		Mono<String> ms4 = webClient.get().uri("http://localhost:9005/").retrieve().bodyToMono(String.class);
+// 		Mono<String> ms4 = webClient.get().uri("sample-service5.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
 // 		Mono<String> ms5 = webClient.get().uri("http://localhost:9006/").retrieve().bodyToMono(String.class);
 // 		Mono<String> ms6 = webClient.get().uri("http://localhost:9007/").retrieve().bodyToMono(String.class);
 // 		Mono<String> ms7 = webClient.get().uri("http://localhost:9008/").retrieve().bodyToMono(String.class);
 // 		Mono<String> ms8 = webClient.get().uri("http://localhost:9009/").retrieve().bodyToMono(String.class);
-		return Mono.zip(ms1, ms2, ms3).flatMap(data -> {
-			return Mono.just("" + data.getT1() + "<br/>" + data.getT2() + "<br/>" + data.getT3() + "<br/>");
+		return Mono.zip(ms1, ms2, ms3, ms4).flatMap(data -> {
+			return Mono.just("" + data.getT1() + "<br/>" + data.getT2() + "<br/>" + data.getT3() + "<br/>" + data.getT4() + "<br/>");
 		});
 // 		return Mono.zip(ms1, ms2, ms3, ms4, ms5, ms6, ms7, ms8).flatMap(data -> {
 // 			return Mono.just("" + data.getT1() + "<br/>" + data.getT2() + "<br/>" + data.getT3() + "<br/>" + data.getT4() + "<br/>" + data.getT5() + "<br/>" + data.getT6() + "<br/>" + data.getT7() + "<br/>" + data.getT8());
