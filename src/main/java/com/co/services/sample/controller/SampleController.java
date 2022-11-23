ec65b63261fdc66f132917770da3d29a6b4ce863
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-11-23 13:49:09 -0500

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.co.services.sample.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,38 +9,65 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class SampleController {
-	Boolean runParallel = true;
+	final Boolean runParallel = true;
 	@GetMapping("/")
-		public String sample1() {
-		WebClient webClient = WebClient.create();
-		if (runParallel) {
-			Mono<String> ms1 = webClient.get().uri("sample-service2-v2.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-			Mono<String> ms2 = webClient.get().uri("sample-service3-v1.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-			Mono<String> ms3 = webClient.get().uri("sample-service4.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-			Mono<String> ms4 = webClient.get().uri("sample-service5.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-			Mono<String> ms5 = webClient.get().uri("sample-service6.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-			Mono<String> ms6 = webClient.get().uri("sample-service7.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-			Mono<String> ms7 = webClient.get().uri("sample-service8.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-			Mono<String> ms8 = webClient.get().uri("sample-service9.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
-		
-			Mono<String> result = Mono.zip(ms1, ms2, ms3, ms4, ms5, ms6, ms7, ms8).flatMap(data -> {
-				return Mono.just("" + data.getT1() + "<br/>" + data.getT2() + "<br/>" + data.getT3() + "<br/>" + data.getT4() + "<br/>" + data.getT5() + "<br/>" + data.getT6() + "<br/>" + data.getT7() + "<br/>" + data.getT8());
-			});
+	public String sample1() {
+		return this.callInterMs(runParallel);
 
-			return result.block();
-		}
-
-		String ms1 = webClient.get().uri("sample-service2-v2.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
-		String ms2 = webClient.get().uri("sample-service3-v1.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
-		String ms3 = webClient.get().uri("sample-service4.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
-		String ms4 = webClient.get().uri("sample-service5.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
-		String ms5 = webClient.get().uri("sample-service6.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
-		String ms6 = webClient.get().uri("sample-service7.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
-		String ms7 = webClient.get().uri("sample-service8.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
-		String ms8 = webClient.get().uri("sample-service9.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
-
-
-		return ms1 + "<br/>" + ms2 + "<br/>" + ms3 + "<br/>" + ms4 + "<br/>" + ms5 + "<br/>" + ms6 + "<br/>" + ms7 + "<br/>" + ms8;
 	}
+
+	@GetMapping("/responseTime")
+	public String sample2() {
+		
+		long endTime = 0;
+ 		long endTime2 = 0;
+
+		long startTime = System.currentTimeMillis(); // Get the start Time
+
+		this.callInterMs(runParallel);
+
+		endTime = System.currentTimeMillis(); // Get the end Time parallel
+
+		long startTime2 = System.currentTimeMillis();
+		this.callInterMs(!runParallel);
+		endTime2 = System.currentTimeMillis(); // Get the end Time sequential
+
+		return new String("Response time(parallel): " + (endTime - startTime) + "ms\n Response time(sequential): "
+				+ (endTime2 - startTime2) + "ms"); // Print the difference in mili seconds
+
+	}
+
+	public String callInterMs(boolean runParallel) {
+			
+			WebClient webClient = WebClient.create();
+			if (runParallel) {
+				Mono<String> ms1 = webClient.get().uri("sample-service2-v2.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
+				Mono<String> ms2 = webClient.get().uri("sample-service3-v1.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
+				Mono<String> ms3 = webClient.get().uri("sample-service4.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
+				Mono<String> ms4 = webClient.get().uri("sample-service5.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
+				Mono<String> ms5 = webClient.get().uri("sample-service6.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
+				Mono<String> ms6 = webClient.get().uri("sample-service7.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
+				Mono<String> ms7 = webClient.get().uri("sample-service8.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
+				Mono<String> ms8 = webClient.get().uri("sample-service9.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class);
+			
+				Mono<String> result = Mono.zip(ms1, ms2, ms3, ms4, ms5, ms6, ms7, ms8).flatMap(data -> {
+					return Mono.just("" + data.getT1() + "<br/>" + data.getT2() + "<br/>" + data.getT3() + "<br/>" + data.getT4() + "<br/>" + data.getT5() + "<br/>" + data.getT6() + "<br/>" + data.getT7() + "<br/>" + data.getT8());
+				});
+
+				return result.block();
+			}
+
+			String ms1 = webClient.get().uri("sample-service2-v2.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
+			String ms2 = webClient.get().uri("sample-service3-v1.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
+			String ms3 = webClient.get().uri("sample-service4.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
+			String ms4 = webClient.get().uri("sample-service5.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
+			String ms5 = webClient.get().uri("sample-service6.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
+			String ms6 = webClient.get().uri("sample-service7.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
+			String ms7 = webClient.get().uri("sample-service8.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
+			String ms8 = webClient.get().uri("sample-service9.charles-mesh.svc.cluster.local:9000/").retrieve().bodyToMono(String.class).block();
+
+
+			return ms1 + "<br/>" + ms2 + "<br/>" + ms3 + "<br/>" + ms4 + "<br/>" + ms5 + "<br/>" + ms6 + "<br/>" + ms7 + "<br/>" + ms8;
+		}
 
 }
